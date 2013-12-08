cb6c864d0eb76f287b389faf9def7c2857eaabf8
Ezra Sharp <ezra@kiwidev.co.nz>
2013-12-09 02:17:53 +1300

    Config Mysql Host for Produciton

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => Config::get('keys.laravel.database.db_name'),
 			'username'  => Config::get('keys.laravel.database.username'),
 			'password'  => Config::get('keys.laravel.database.password'),
