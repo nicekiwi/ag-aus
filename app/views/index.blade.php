4677213912310a52fd3cf291e25e4d1884aa068e
Ezra Sharp <ezra@kiwidev.co.nz>
2014-04-25 12:16:14 +1200

    Sync Update

---
+++ app/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="jumbotron">
+		<h1>Hello, world!</h1>
+		<p>...</p>
+		<p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+	</div>
+
+	<div class="panel panel-default">
+	  <div class="panel-heading">
+	    <h3 class="panel-title"><i class="fa fa-calendar-o"></i> Upcoming Events</h3>
+	  </div>
+	  <div class="panel-body">
+	    20/12/2014 - JukeBox Night
+	  </div>
+	</div>
+
+@stop
