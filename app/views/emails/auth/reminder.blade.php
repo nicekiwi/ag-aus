09ba3bd9c7be84301d5e67d42028bdf740166d7a
Ezra Sharp <ezra@kiwidev.co.nz>
2014-05-17 11:44:38 +1200

    Reminder Email Templated

---
+++ app/views/emails/auth/reminder.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.email')
+
+@section('content')
+
+<h2>Password Reset</h2>
+
+<div>
+	To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.
+</div>
+
+@stop
