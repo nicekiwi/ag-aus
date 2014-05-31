d187dbeff3a27e913ad23630e75f811acc7529bc
Ezra Sharp <ezra@kiwidev.co.nz>
2014-06-01 11:23:54 +1200

    Add Expenses Table

---
+++ app/database/migrations/2014_04_04_000002_create_expenses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpensesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('expenses', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title');
+
+			$table->text('desc')->nullable();
+			$table->integer('amount');
+
+			$table->softDeletes();
+			$table->integer('updated_by')->nullable();
+			$table->integer('created_by')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('expenses');
+	}
+
+}
